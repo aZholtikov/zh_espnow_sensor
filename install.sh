32bace61c20bd46a1310cc059df884ff626da567
Alexey Zholtikov <git@zh.com.ru>
2024-03-03 09:01:25 +0300

    Minor changes

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 git clone -b esp32 --recursive http://git.zh.com.ru/alexey.zholtikov/zh_espnow_sensor.git
-cd zh_espnow_switch
+cd zh_espnow_sensor
 git submodule init
 git submodule update --remote
